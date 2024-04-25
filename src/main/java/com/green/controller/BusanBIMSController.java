d89041cf4acd60b63a513ab79e4df25082f893cb
birdcross <birdcross10@gmail.com>
2024-04-25 12:47:12 +0900

    axios03

--- src/main/java/com/green/controller/BusanBIMSController.java
+++ src/main/java/com/green/controller/BusanBIMSController.java
@@ -11,21 +11,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.green.dto.BusInfoVo;
+
 @Controller
 public class BusanBIMSController {
 	
 	@RequestMapping("/GetBus")
 	@ResponseBody
-	public String getbus() throws IOException {
+	public String getbus(
+			BusInfoVo dto
+			) throws IOException {
 		
 		String serviceKey = "3DxuF2sDKbF2mEVUby63%2FBKgmiqiqHEGx%2F7vcbocHxfE82kYG7KtAy4xNyjHe%2FtvweZr8PRYyQHKeMWCH6umtA%3D%3D";
 		
 		StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/6260000/BusanBIMS/busStopList"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + serviceKey); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode("부산시청", "UTF-8")); /*정류소 명*/
-        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode("13708", "UTF-8")); /*정류소 번호*/
+        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode(dto.getPageNo(), "UTF-8")); /*페이지번호*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode(dto.getNumOfRows(), "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("bstopnm","UTF-8") + "=" + URLEncoder.encode(dto.getBstopnm(), "UTF-8")); /*정류소 명*/
+        urlBuilder.append("&" + URLEncoder.encode("arsno","UTF-8") + "=" + URLEncoder.encode(dto.getArsno(), "UTF-8")); /*정류소 번호*/
+        
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -44,8 +49,9 @@ public class BusanBIMSController {
         }
         rd.close();
         conn.disconnect();
-        System.out.println(sb.toString());
-		return sb.toString();
-	}
+        System.out.println(sb.toString());   
+        
+        return sb.toString();
+	}	
 	
 }
